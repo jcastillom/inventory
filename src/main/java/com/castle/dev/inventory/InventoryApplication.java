6704a30f1bc8675312f8391131d4f76d87dad36a
vonec <vonec@vone_castillo>
2024-11-18 20:15:22 -0600

    prueba ajax fallida por cors

--- src/main/java/com/castle/dev/inventory/InventoryApplication.java
+++ src/main/java/com/castle/dev/inventory/InventoryApplication.java
@@ -2,6 +2,9 @@ package com.castle.dev.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -9,5 +12,18 @@ public class InventoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
+	
+//	@Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*");
+//            }
+//        };
+//    }
 
 }
--- src/main/java/com/castle/dev/inventory/controller/TestSpringController.java
+++ src/main/java/com/castle/dev/inventory/controller/TestSpringController.java
@@ -1,8 +1,14 @@
 package com.castle.dev.inventory.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @Controller
 public class TestSpringController {
 	
@@ -10,6 +16,13 @@ public class TestSpringController {
 	public String hola() {
 		return "hola";
 	}
+	
+	@GetMapping("/obtenerDatos")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String hola2(Model model) {
+		return "cadena de controller";
+	}
+	
 	@GetMapping("/ejemplo")
 	public String ejemplo() {
 		return "ejemplo";
