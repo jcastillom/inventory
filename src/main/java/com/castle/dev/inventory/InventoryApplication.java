af27412be6060ec667da6f2ef9f44b95d68cee76
José Castillo Miramar <jcastillo.java@gmail.com>
2024-12-14 21:15:05 -0600

    Proyecto base prueba

--- src/main/java/com/castle/dev/inventory/InventoryApplication.java
+++ src/main/java/com/castle/dev/inventory/InventoryApplication.java
@@ -2,9 +2,6 @@ package com.castle.dev.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -12,18 +9,20 @@ public class InventoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
-	
-//	@Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*")
-//                .allowedHeaders("*");
-//            }
-//        };
-//    }
+//	
+//	 @Bean
+//	 public WebMvcConfigurer corsConfigurer() {
+//	 	return new WebMvcConfigurer() {
+//	 		@Override
+//	 		public void addCorsMappings(CorsRegistry registry) {
+//	 			registry.addMapping("/**")
+//	 				.allowedOrigins("localhost:8080")
+//	 				.allowedMethods("GET", "POST", "OPTIONS", "PUT", "PATCH")
+//	 				.allowedHeaders("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization")
+//	 				.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+//	 				.maxAge(0);
+//	 		}
+//	 	};
+//	 }
 
 }
